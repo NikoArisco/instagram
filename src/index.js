ce9cea7fe74bde206875deb8464c6794f8f29708
Niko Felix Chandra Arisco <146053558+NikoArisco@users.noreply.github.com>
2025-08-13 15:31:32 +0700

    Buat tampilan Signup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import FirebaseContext from "./context/firebase";
+import { firebase, FieldValue } from "./lib/firebase";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
     <App />
-  </React.StrictMode>
+  </FirebaseContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
